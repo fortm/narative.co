fec39b193ca406e5b597acef533e0d19303d31f9
Dennis Brotzky <dennis.brotzky@poweredbygrow.com>
2018-05-02 18:48:33 -0700

    new file

---
+++ src/modules/forms/PhoneForm.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+import { Formik, Form as FormikForm, Field } from 'formik'
+import { Button, Container, Form } from '@components'
+
+const FormHeader = styled.h3`
+  font-size: 1.8rem;
+  margin-bottom: 3rem;
+  color: ${props => props.theme.colors.grey};
+`
+
+const FormSection = styled.fieldset`
+  margin-bottom: 5rem;
+`
+
+const StyledFormikForm = styled(FormikForm)`
+  width: 46rem;
+  align-self: flex-end;
+  position: relative;
+  top: 13%;
+`
+
+const RadioOptions = [
+  {
+    label: 'Branding',
+    id: 'branding',
+    value: 'Branding',
+  },
+  {
+    label: 'Product and Web',
+    id: 'productAndWeb',
+    value: 'Product and Web',
+  },
+  {
+    label: 'Editorial',
+    id: 'editorial',
+    value: 'Editorial',
+  },
+  {
+    label: 'Other',
+    id: 'other',
+    value: 'Other',
+  },
+]
+
+const validate = (values, props) => {
+  let errors = {}
+
+  if (!values.phone) {
+    errors.phone = 'Required'
+  }
+
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  return errors
+}
+
+class ContactForm extends Component {
+  handleSubmit(event) {
+    console.log(event)
+  }
+
+  render() {
+    return (
+      <Formik
+        onSubmit={this.handleSubmit}
+        validate={validate}
+        render={props => (
+          <StyledFormikForm>
+            <FormSection>
+              <Field component={Form.Phone} label="123 456-7890" name="phone" />
+            </FormSection>
+            <Button text="Submit" />
+          </StyledFormikForm>
+        )}
+      />
+    )
+  }
+}
+
+export default ContactForm
