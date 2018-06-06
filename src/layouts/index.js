4049640210af9a4cf07ccb46a956f3eb7a7be656
brotzky <brotzky@gmail.com>
2018-06-06 08:28:07 -0700

    Fixing mobile overflow

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import styled, { ThemeProvider } from 'styled-components'
-import { globalStyles, theme } from '@styles'
+import { globalStyles, media, theme } from '@styles'
 
 // Injecting global styles and reset
 globalStyles()
@@ -24,6 +24,10 @@ const WebContainer = styled.div`
   min-height: 100vh;
   width: 100vw;
   overflow-x: hidden;
+
+  ${media.tablet`
+    overflow: initial;
+  `};
 `
 
 const Layout = ({ children, data }) => (
